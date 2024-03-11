2e9945931f2d7822e5f72cea5ee55af9300b6493
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-03-11 17:51:18 +0600

    Features: Counter Slice

--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  counter: 0,
+	counter: 0,
 };
 
 const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {},
+	name: "counter",
+	initialState,
+	reducers: {
+		increment: (state) => {
+			state.counter += 1;
+		},
+		decrement: (state) => {
+			state.counter -= 1;
+		},
+	},
 });
+
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
